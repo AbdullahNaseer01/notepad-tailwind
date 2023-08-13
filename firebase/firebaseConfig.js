e0f3d1ecb58500e31f60534b2c7a2e12796896f1
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-08-13 18:46:48 +0500

    tutorial

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCl_ShoJcK3gfyrbyQVgOslNeixNNyBmWI",
   authDomain: "task-management-app-16ef6.firebaseapp.com",
@@ -17,8 +14,8 @@ const firebaseConfig = {
   measurementId: "G-ZGBT8W3HG6"
 };
 
-// Initialize Firebase
 
 export const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app)
+export const auth = getAuth(app)
 const analytics = getAnalytics(app);
