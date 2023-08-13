86ceb0e5df64e919c2a2b47d4c6eb765814069d8
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-08-13 19:40:03 +0500

    signup form inetialized

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,105 +1,65 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 
 const page = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex justify-center items-center">
-    <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-sm">
-      <h2 className="text-2xl text-center font-bold mb-4">
-        {!signUp ? "Sign-In" : "Sign-Up"}
-      </h2>
-      <form onSubmit={handleAuth}>
-        {signUp && (
-          <>
-            <div className="mb-4">
-              <input
-                type="text"
-                className="w-full border rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                placeholder="First Name"
-                name="firstName"
-                value={firstName}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                type="text"
-                className="w-full border rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                placeholder="Last Name"
-                name="lastName"
-                value={lastName}
-                onChange={handleChange}
-              />
-            </div>
-          </>
-        )}
+    <div className="flex items-center justify-center h-screen">
+         <div className="bg-white p-8 rounded shadow-md max-w-sm w-full">
+      <h1 className="text-2xl font-semibold mb-4">Sign Up</h1>
+      <form>
         <div className="mb-4">
+          <label
+            htmlFor="username"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
           <input
             type="email"
-            className="w-full border rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            placeholder="Email"
+            id="email"
             name="email"
-            value={email}
-            onChange={handleChange}
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           />
         </div>
         <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
           <input
             type="password"
-            className="w-full border rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            placeholder="Password"
+            id="password"
             name="password"
-            value={password}
-            onChange={handleChange}
+            className="mt-1 block w-full rounded-md border-gray-600 shadow-sm focus:border-indigo-600 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           />
         </div>
-        {signUp && (
-          <div className="mb-4">
-            <input
-              type="password"
-              className="w-full border rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-400"
-              placeholder="Confirm Password"
-              name="confirmPassword"
-              value={confirmPassword}
-              onChange={handleChange}
-            />
-          </div>
-        )}
-        <div className="text-center">
-          <button
-            className={`py-2 px-4 rounded-lg ${!signUp ? "bg-blue-500 text-white" : "bg-green-500 text-white"}`}
-            type="submit"
-          >
-            {!signUp ? "Sign-in" : "Sign-up"}
-          </button>
-        </div>
+        <button
+          type="submit"
+          className="w-full py-2 px-4 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        >
+          Sign Up
+        </button>
       </form>
-      <div className="text-center mt-4">
-        {!signUp ? (
-          <p className="text-sm">
-            Don't have an account?{" "}
-            <span
-              className="text-blue-500 cursor-pointer"
-              onClick={() => setSignUp(true)}
-            >
-              Sign Up
-            </span>
-          </p>
-        ) : (
-          <p className="text-sm">
-            Already have an account?{" "}
-            <span
-              className="text-blue-500 cursor-pointer"
-              onClick={() => setSignUp(false)}
-            >
-              Sign In
-            </span>
-          </p>
-        )}
-      </div>
     </div>
-  </div>
-  )
-}
+    </div>
+   
+  );
+};
 
-export default page
+export default page;
