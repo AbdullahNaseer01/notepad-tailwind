eabf1535947d0f546e3339c579383ac20aa56e8b
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-08-15 13:39:12 +0500

    Auth.js file completed

--- firebase/Auth.js
+++ firebase/Auth.js
@@ -1,19 +1,58 @@
-import { createContext , useContext , useState , useEffect } from "react";
-import { onAuthStateChanged , signOut as authSignOut} from 'firebase/auth'
+import { createContext, useContext, useState, useEffect } from "react";
+import { onAuthStateChanged, signOut as authSignOut } from "firebase/auth";
+import { auth } from "./firebaseConfig";
 
+const AuthUserContext = createContext({
+  authUser: null,
+  isLoading: true,
+});
 
-const AuthUserContext = createContext ({
-    authUser : null,
-    isLoading : true
-})
+export default function useFirebaseAuth() {
+  const [authUser, setAuthUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-export const authUserProvider = ({children}) =>{
-return(
-    <AuthUserContext.Provider value={{}}>
-        {children}
-    </AuthUserContext.Provider>
+  const clear = () => {
+    setAuthUser(null);
+    setIsLoading(false);
+  };
 
-)
+  const authStateChanged = async (user) => {
+    setIsLoading(true);
+    if (!user) {
+      clear();
+      return;
+    }
+    setAuthUser({
+      uid: user.uid,
+      email: user.email,
+      username: user.displayName,
+    });
+    setIsLoading(false);
+  };
+
+  const signOut = () => {
+    authSignOut(auth).then(() => clear());
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, authStateChanged);
+    return () => unSubscribe();
+  }, []);
+
+  return {
+    authUser,
+    isLoading,
+    setAuthUser,
+    signOut,
+  };
 }
 
-export const useAuth = () => useContext(AuthUserContext)
+export const authUserProvider = ({ children }) => {
+  const auth = useFirebaseAuth();
+
+  return (
+    <AuthUserContext.Provider value={auth}>{children}</AuthUserContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthUserContext);
