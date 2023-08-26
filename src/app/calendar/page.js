a6ad92faf6e86e7fa2f4dc254f08a12a9a494f6d
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-08-26 23:13:39 +0500

    new page added calendar

---
+++ src/app/calendar/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>calandar page under construction  </div>
+  )
+}
+
+export default page
--- src/app/components/AsideMenu.jsx
+++ src/app/components/AsideMenu.jsx
@@ -8,9 +8,6 @@ import { useRouter, usePathname } from 'next/navigation';
 const AsideMenu = ({ isMenuToggle, signOut }) => {
   const router = useRouter();
   const pathname = usePathname()
-  const checker = () => {
-    console.log(router.pathname)
-  }
   return (
     <div className={` sm:inline ${isMenuToggle ? 'block' : 'hidden'}`}>
       <aside className="fixed inset-y-0 left-0 bg-white shadow-md max-h-screen w-60">
@@ -45,7 +42,6 @@ const AsideMenu = ({ isMenuToggle, signOut }) => {
                   <Link
                     href="/upcoming"
                     className={`flex items-center ${pathname === '/upcoming' ? "bg-yellow-200 rounded-xl font-bold text-sm text-yellow-900 py-3 px-4" : " bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4"}`}
-
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -61,10 +57,9 @@ const AsideMenu = ({ isMenuToggle, signOut }) => {
                   </Link>
                 </li>
                 <li>
-                  <a
-                    href=""
-                    onClick={checker}
-                    className="flex bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4"
+                  <Link
+                    href="/calendar"
+                    className={`flex items-center ${pathname === '/calendar' ? "bg-yellow-200 rounded-xl font-bold text-sm text-yellow-900 py-3 px-4" : " bg-white hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4"}`}
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -76,8 +71,8 @@ const AsideMenu = ({ isMenuToggle, signOut }) => {
                     >
                       <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z" />
                     </svg>
-                    Projects
-                  </a>
+                    Calendar
+                  </Link>
                 </li>
                 <li>
                   <a
